b365214622c6d98184f28aab0229ac0574ebc096
FahadQ <Fhdq2003@email.com>
2025-08-18 16:37:44 +0300

    added changed that will make the server work

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,19 +3,6 @@ const router = express.Router();
 const UserController = require("../controllers/User");
 const secureRoute = require('../middleware/secureRoute')
 
-const storage = multer.memoryStorage
-    ({
-        destination: (req, file, cb) =>
-        {
-            cb(null, './public/images/');
-        },
-        filename: (req, file, cb) =>
-        {
-            cb(null, file.originalname);
-        }
-    });
-const upload = multer({ storage });
-
 router.post("/new", UserController.createUser);//create new user
 router.put("/:id",secureRoute ,UserController.updateUser);//Update the user
 router.get("/", secureRoute, UserController.allUsers);//all user info
